fedbd0620c63eb703c70a02d5d58e4760679fac4
Ajit Singh <32074655+as-ajitsingh@users.noreply.github.com>
2021-09-22 16:02:47 +0530

    Changed text and build

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Github Deployed App
+          Welcome to Github Deployed App Now !!
         </p>
         <a
           className="App-link"
